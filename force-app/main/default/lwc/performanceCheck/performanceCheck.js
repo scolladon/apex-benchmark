39ade31d4f0c519638f3809e850429b9e224b9c5
Sébastien Colladon <colladonsebastien@gmail.com>
2024-07-04 15:34:15 +0200

    feat: add datatable

--- force-app/main/default/lwc/performanceCheck/performanceCheck.js
+++ force-app/main/default/lwc/performanceCheck/performanceCheck.js
@@ -1,14 +1,65 @@
-import { LightningElement } from "lwc";
+import { LightningElement, track } from "lwc";
 
 import staticPerformance from "@salesforce/apex/PerformanceChecker.staticPerformance";
 import objectPerformance from "@salesforce/apex/PerformanceChecker.objectPerformance";
 
+const columns = [
+  { label: "Type", fieldName: "type" },
+  { label: "Metric", fieldName: "metric" },
+  { label: "Min", fieldName: "min", type: "number" },
+  { label: "Max", fieldName: "max", type: "number" },
+  { label: "Mean", fieldName: "mean", type: "number" },
+  { label: "Median", fieldName: "median", type: "number" }
+];
+
 export default class PerformanceCheck extends LightningElement {
+  isbuttonDisabled = false;
+
+  iteration = 50000;
+
+  @track
+  data = [];
+
+  columns = columns;
+
+  get staticClickDisabled() {
+    return this.isStaticClickDisabled;
+  }
+
+  get objectClickDisabled() {
+    return this.isObjectClickDisabled;
+  }
+
   async staticClick() {
-    await staticPerformance();
+    this.isbuttonDisabled = true;
+    const executionResult = await staticPerformance({ iteration: this.iteration });
+    const type = `Static(${this.iteration} iterations)`;
+    this.addStat(type, "ms", executionResult.cpuMetrics);
+    this.addStat(type, "bytes", executionResult.heapSizeMetrics);
+    this.data = [...this.data];
+    this.isbuttonDisabled = false;
   }
 
   async objectClick() {
-    await objectPerformance();
+    this.isbuttonDisabled = true;
+    const executionResult = await objectPerformance({ iteration: this.iteration });
+    const type = `Object(${this.iteration} iterations)`;
+    this.addStat(type, "ms", executionResult.cpuMetrics);
+    this.addStat(type, "bytes", executionResult.heapSizeMetrics);
+    this.data = [...this.data];
+    this.isbuttonDisabled = false;
+  }
+
+  handleIterationChange(e) {
+    this.iteration = e.detail.value;
+  }
+
+  addStat(type, metric, stats) {
+    this.data.push({
+      id: `${type}-${metric}-${this.iteration}-${Date.now()}`,
+      ...stats,
+      type,
+      metric
+    });
   }
 }
